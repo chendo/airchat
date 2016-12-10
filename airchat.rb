0d57b0d3212d4fd6121d535d911afba75bbda4cf
Jack Chen (chendo) <github@chen.do>
2016-12-10 14:41:44 +1100

    Basic protocol with message dedup

--- airchat.rb
+++ airchat.rb
@@ -4,6 +4,8 @@ Bundler.require
 require "ffi/pcap"
 require "ffi/packets"
 require "readline"
+require "securerandom"
+require "json"
 
 module FFI::Packets
   module Ipv6
@@ -20,26 +22,103 @@ module FFI::Packets
   end
 end
 
+# Airchat protocol
+# JSON
+# from: 'nick'
+# id: uuid
+# event: [join/leave/msg/ack]
+# data: {msg: }
+
+Thread.abort_on_exception = true
+
+class Airchat
+  class Message < Struct.new(:id, :from, :event, :data)
+    def self.parse(json)
+      data = JSON.parse(json)
+      id, from, event, data = [:id, :from, :event, :data].map do |key|
+        data.fetch(key.to_s)
+      end
+      new(id, from, event, data)
+    end
+
+    def self.create(from:, event:, data: nil)
+      new(SecureRandom.uuid, from, event, data)
+    end
+
+    def self.msg(from:, msg:)
+      create(from: from, event: 'msg', data: msg)
+    end
+
+    def self.nick(from:, new_nick:)
+      create(from: from, event: 'nick', data: new_nick)
+    end
+
+    def self.join(from:, data: nil)
+      create(from: from, event: 'join')
+    end
+
+    def self.leave(from:, data: nil)
+      create(from: from, event: 'leave')
+    end
+
+    def to_json
+      JSON.dump({
+        id: id,
+        from: from,
+        event: event,
+        data: data
+      })
+    end
+  end
+end
+
 class Airchat
   def initialize(port: 1337, preamble: '__AIRCHAT:')
     @ip_to_host = {}
     @port = port
     @preamble = preamble
-    @messages = []
+    @seen_messages = []
+    @nick = "Guest#{rand(1000)}"
+    @socket = UDPSocket.new(Socket::AF_INET6)
+    @socket.connect('ff02::fb%awdl0', @port)
+  end
+
+  def prompt_text
+    ">> "
+  end
+
+  def write(msg)
+    @socket.write("#{@preamble}#{msg.to_json}")
+  end
+
+  def send_msg(type, **args)
+    msg = Message.send(type, **{from: @nick}.merge(args))
+    3.times { write(msg) }
   end
 
   def run
+    at_exit do
+      send_msg(:leave)
+    end
+
     listen_thr = Thread.new do
       listen
     end
 
-    socket = UDPSocket.new(Socket::AF_INET6)
-    socket.connect('ff02::fb%awdl0', @port)
+    send_msg(:join)
+
     while true
-      line = Readline.readline(">> ")
+      line = Readline.readline(prompt_text).chomp
+      print "\033[1A\033[K"
 
       if line.length > 0
-        socket.puts("#{@preamble}#{line}")
+
+        if line =~ /\/nick (\w{1,32})/
+          send_msg(:nick, new_nick: $1)
+          @nick = $1
+        else
+          send_msg(:msg, msg: line)
+        end
       end
     end
   end
@@ -76,11 +155,43 @@ class Airchat
     end
   end
 
+  def output(line)
+    puts "\r[#{Time.now.strftime("%H:%M:%S")}] #{line}"
+  end
+
+  def status_output(line)
+    output("* #{line}")
+  end
+
   def handle_message(from:, data:)
     if data =~ /^#{@preamble}/
       last_4 = from[-4..-1].unpack("H*").first
-      host = @ip_to_host[from] || 'unknown'
-      puts "[#{host}/#{last_4}] #{data.sub(@preamble, '')}"
+
+      json = data.sub(@preamble, '')
+      msg = Message.parse(json)
+
+      if @seen_messages.include?(msg.id)
+        return
+      end
+
+      @seen_messages << msg.id
+      while @seen_messages.count > 100
+        @seen_messages.delete_at(0)
+      end
+
+      nick = msg.from || @ip_to_host[from] || 'unknown'
+      from = "#{nick}@#{last_4}"
+      case msg.event
+      when 'join'
+        status_output "#{from} has joined"
+      when 'leave'
+        status_output "#{from} has left"
+      when 'msg'
+        output "[#{from}] #{msg.data}"
+      when 'nick'
+        status_output "#{from} changed nick to #{msg.data}"
+      end
+      print prompt_text
     end
   end
 
